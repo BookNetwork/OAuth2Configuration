0f8aad0622a64c8377b2781af7510e7519549d76
BookFun <bookfun388@gmail.com>
2019-08-21 01:41:27 +0530

    initial commit and sample page navigateion test implementation

---
+++ src/main/java/com/sliit/ssd/controller/SampleController.java
@@ -0,0 +1,16 @@
+package com.sliit.ssd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class SampleController {
+	
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String  sample() {
+		return "index.html";
+	}
+
+}
